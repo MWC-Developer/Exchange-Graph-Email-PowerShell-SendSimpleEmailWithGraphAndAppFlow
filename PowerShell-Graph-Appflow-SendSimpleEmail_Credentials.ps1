b9127994a026dfea46af3d0018a538054239e3ae
Daniel Bagley <dseph@users.noreply.github.com>
2026-09-25 01:10:10 -0400

    Create PowerShell script for email credentials setup
    Added configuration parameters for Tenant ID, Client ID, and Client Secret.

---
+++ PowerShell-Graph-Appflow-SendSimpleEmail_Credentials.ps1
@@ -0,0 +1,11 @@
+#PowerShell-Graph-Appflow-SendSimpleEmail_Credentials.ps1
+
+# =========================
+# CONFIG
+# =========================
+$TenantId     = "YOUR_TENANT_ID"        # TODO:  Change this to the Tenant Id
+$ClientId     = "YOUR_CLIENT_ID"        # TODO:  Change this to the Client Id/Application Id
+$ClientSecret = "YOUR_CLIENT_SECRET"    # TODO:  Change this to the Client Secret VALUE.
+
+# Note: You can inject this file's code into another by using the call below:
+# . "$PSScriptRoot\PowerShell-Graph-Appflow-SendSimpleEmail_Credentials.ps1" 
